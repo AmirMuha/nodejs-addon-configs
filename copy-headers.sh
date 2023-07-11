dec8863ef0e5226753d0067d4b0542f831e21731
amirmuha <mirzaii4046@gmail.com>
2023-07-11 08:39:21 +0330

    fix: fixed node-addon-api entry point for copy

--- copy-headers.sh
+++ copy-headers.sh
@@ -1,4 +1,4 @@
 npm i -g node-addon-api
 sudo cp -r /home/amirmuha/.nvm/versions/node/$(node -v)/include/node /usr/include
 sudo mkdir -p /usr/include/napi
-sudo cp -r /home/amirmuha/.nvm/versions/node/$(node -v)/lib/node_modules/node-addon-api/**/* /usr/include/napi
+sudo cp -r /home/amirmuha/.nvm/versions/node/$(node -v)/lib/node_modules/node-addon-api/* /usr/include/napi
