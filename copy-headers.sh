ef2931e927ba1865222fa167b0785e5f687b9de5
amirmuha <mirzaii4046@gmail.com>
2023-07-11 08:36:22 +0330

    fix: fixed node -v placeholder

--- copy-headers.sh
+++ copy-headers.sh
@@ -1,6 +1,4 @@
-NODE_VERSION= node -v
-echo $NODE_VERSION
 npm i -g node-addon-api
-sudo cp -r /home/amirmuha/.nvm/versions/node/$NODE_VERSION/include/node /usr/include
+sudo cp -r /home/amirmuha/.nvm/versions/node/$(node -v)/include/node /usr/include
 sudo mkdir -p /usr/include/napi
-sudo cp -r /home/amirmuha/.nvm/versions/node/$NODE_VERSION/lib/node_modules/node-addon-api/**/* /usr/include/napi
+sudo cp -r /home/amirmuha/.nvm/versions/node/$(node -v)/lib/node_modules/node-addon-api/**/* /usr/include/napi
