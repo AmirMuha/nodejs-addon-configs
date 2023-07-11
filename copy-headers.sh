33adefc00c77261652da80d3c7008fe5f5cbde83
amirmuha <mirzaii4046@gmail.com>
2023-07-11 08:24:03 +0330

    fix: fixed shell commands to copy node-addon-api and node header files

--- copy-headers.sh
+++ copy-headers.sh
@@ -1,3 +1,6 @@
-cp -r /home/amirmuha/.nvm/versions/node/v18.15.0/include/node /usr/include
-mkdir -p /usr/include/napi
-cp -r https://github.com/nodejs/node-addon-examples/**/* /usr/include/napi
+NODE_VERSION= node -v
+echo $NODE_VERSION
+npm i -g node-addon-api
+sudo cp -r /home/amirmuha/.nvm/versions/node/$NODE_VERSION/include/node /usr/include
+sudo mkdir -p /usr/include/napi
+sudo cp -r /home/amirmuha/.nvm/versions/node/$NODE_VERSION/lib/node_modules/node-addon-api/**/* /usr/include/napi
